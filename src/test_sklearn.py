9b2593cfdfb6992b0bf6d66e1a40d2a461da305c
4lexks <99203252+4lexks@users.noreply.github.com>
2026-06-18 22:30:51 +0200

    Delete src/test_sklearn.py

--- src/test_sklearn.py
+++
@@ -1,9 +0,0 @@
-import sklearn
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
-
-print("scikit-learn version:", sklearn.__version__)
-
-X, y = load_iris(return_X_y=True)
-clf = LogisticRegression(max_iter=200).fit(X, y)
-print("accuracy:", clf.score(X, y))
