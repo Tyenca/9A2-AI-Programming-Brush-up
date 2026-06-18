d98093816e5c1424cc13203159b5c4128133618d
4lexks <99203252+4lexks@users.noreply.github.com>
2026-06-18 16:16:15 +0200

    added cm to .py for local running and random state to all base models

--- src/base_rf.py
+++ src/base_rf.py
@@ -1,3 +1,4 @@
+#import packages
 from utils import normalize_dataset, get_datasets, prepare_sklearn_data
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, cohen_kappa_score, confusion_matrix, ConfusionMatrixDisplay
@@ -11,16 +12,17 @@ mean, std = normalize_dataset()
 #Finish normalizing the data
 train_data, val_data, test_data = get_datasets(mean=mean, std=std)
 #Flatten the images, because sklearn models expects 1D feature vectors, not 2D images
-# Create new flat datasets with normalized pixel values
+#Create new flat datasets with normalized pixel values
 X_train, y_train = prepare_sklearn_data(train_data)
 X_val,   y_val   = prepare_sklearn_data(val_data)
 X_test,  y_test  = prepare_sklearn_data(test_data)
 
-# Train a random forest model 
-#For hyperparameter tuning, tried adding class_weight "balanced"
+#Train a random forest model 
+#For hyperparameter tuning, we tried adding class_weight "balanced"
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced') # class_weight='balanced' to handle class imbalance in the dataset
 rf_model.fit(X_train, y_train)
 
+#validation
 rf_val_preds = rf_model.predict(X_val)
 rf_val_acc = accuracy_score(y_val, rf_val_preds)
 print("VALIDATION")
@@ -30,14 +32,24 @@ print(f"Average mean absolute error (AMAE): {amae(y_val, rf_val_preds):.4f}")
 print(f"Maximum mean absolute error (MMAE): {mmae(y_val, rf_val_preds):.4f}")
 print(f"Quadratic weighted kappa: {cohen_kappa_score(y_val, rf_val_preds, weights='quadratic'):.4f}\n")
 
+cm = confusion_matrix(y_val, rf_val_preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0,1,2,3,4])
+disp.plot()
+plt.title("Random Forest — validation set")
+plt.show()
+
+#testing
 rf_test_preds = rf_model.predict(X_test)
 rf_test_acc = accuracy_score(y_test, rf_test_preds)
 print("TEST")
-print("TEST results Random forest")
 print(f"RF test accuracy: {rf_test_acc:.4f}")
 print(f"One-off accuracy: {accuracy_off1(y_test, rf_test_preds):.4f}")
 print(f"Average mean absolute error (AMAE): {amae(y_test, rf_test_preds):.4f}")
 print(f"Maximum mean absolute error (MMAE): {mmae(y_test, rf_test_preds):.4f}")
 print(f"Quadratic weighted kappa: {cohen_kappa_score(y_test, rf_test_preds, weights='quadratic'):.4f}\n")
 
-
+cm = confusion_matrix(y_test, rf_test_preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0,1,2,3,4])
+disp.plot()
+plt.title("Random Forest — test set")
+plt.show()
--- src/base_svc.py
+++ src/base_svc.py
@@ -12,17 +12,17 @@ mean, std = normalize_dataset()
 #Finish normalizing the data
 train_data, val_data, test_data = get_datasets(mean=mean, std=std)
 #Flatten the images, because sklearn models expects 1D feature vectors, not 2D images
-# Create new flat datasets with normalized pixel values
+#Create new flat datasets with normalized pixel values
 X_train, y_train = prepare_sklearn_data(train_data)
 X_val,   y_val   = prepare_sklearn_data(val_data)
 X_test,  y_test  = prepare_sklearn_data(test_data)
 
 
-# Train a linear SVM model 
-C_values = [0.001, 0.01, 0.1, 1.0, 10.0]
+#Train a linear SVM model 
+C_values = [0.001, 0.01, 0.1, 1.0, 10.0] #Hyperparameter tuning values
 best_C, best_svc_acc = None, -1.0  # initialize variables to assure first loop will score the lowest c value. next loop will always update because it will score higher than -1
 
-# Find best performing Regularization parameter C for linear SVM model
+#Find best performing Regularization parameter C for linear SVM model
 for C in C_values:
     # Training model with various c values and saving the best
     svc = LinearSVC(C=C, max_iter=7000, random_state=42, class_weight='balanced') # class_weight='balanced' to handle class imbalance in the dataset
@@ -31,12 +31,14 @@ for C in C_values:
     if acc > best_svc_acc:
         best_svc_acc, best_C = acc, C
 
-# print(f"Best C: {best_C}")
+#Prints the best performing C
+print(f"Best C: {best_C}")
 
 # Retrain with best C
 svc_model = LinearSVC(C=best_C, max_iter=7000, random_state=42, class_weight='balanced') # class_weight='balanced' to handle class imbalance in the dataset
 svc_model.fit(X_train, y_train)
 
+#validation
 svc_val_preds = svc_model.predict(X_val) 
 svc_val_acc = accuracy_score(y_val, svc_val_preds)
 print("VALIDATION")
@@ -46,6 +48,13 @@ print(f"Average mean absolute error (AMAE): {amae(y_val, svc_val_preds):.4f}")
 print(f"Maximum mean absolute error (MMAE): {mmae(y_val, svc_val_preds):.4f}") 
 print(f"Quadratic weighted kappa: {cohen_kappa_score(y_val, svc_val_preds, weights='quadratic'):.4f}\n")
 
+cm = confusion_matrix(y_val, svc_val_preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0,1,2,3,4])
+disp.plot()
+plt.title("SVC — validation set")
+plt.show()
+
+#testing
 svc_test_preds = svc_model.predict(X_test) 
 svc_test_acc = accuracy_score(y_test, svc_test_preds)
 print("TEST")
@@ -54,3 +63,9 @@ print(f"One-off accuracy: {accuracy_off1(y_test, svc_test_preds):.4f}")
 print(f"Average mean absolute error (AMAE): {amae(y_test, svc_test_preds):.4f}")
 print(f"Maximum mean absolute error (MMAE): {mmae(y_test, svc_test_preds):.4f}") 
 print(f"Quadratic weighted kappa: {cohen_kappa_score(y_test, svc_test_preds, weights='quadratic'):.4f}\n")
+
+cm = confusion_matrix(y_test, svc_test_preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0,1,2,3,4])
+disp.plot()
+plt.title("SVC — test set")
+plt.show()
